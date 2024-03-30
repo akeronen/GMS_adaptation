43d49894106b7fa3d7ed48b80385ed13974fefdf
akeronen <69987790+akeronen@users.noreply.github.com>
2024-03-30 18:44:59 +0200

    Add files via upload

---
+++ instance_change.py
@@ -0,0 +1,12 @@
+from pygame.sprite import Group, GroupSingle
+
+def instance_change(self, obj, perf, sprGroup):
+    if perf == True:
+        self.destroy()
+    if isinstance(sprGroup, GroupSingle):
+        sprGroup.add(obj)
+    elif isinstance(sprGroup, Group):
+        # ToDo
+        pass
+    if perf == True:
+        obj.create()
