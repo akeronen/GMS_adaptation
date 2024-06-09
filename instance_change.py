d9be10dce2ae8c1ac1ddc5562daac8387a0b6f0b
akeronen <69987790+akeronen@users.noreply.github.com>
2024-06-09 14:33:40 +0300

    Added sprite group change logic

--- instance_change.py
+++ instance_change.py
@@ -1,13 +1,16 @@
 from pygame.sprite import Group, GroupSingle
 
+# https://stackoverflow.com/questions/70516673/how-does-pygame-sprite-group-know-which-sprite-group-to-call-also-calling-the
+# Sprite.kill() removes the sprite from all of its groups
+
 def instance_change(self, obj, perf, sprGroup):
     if perf == True:
         self.destroy()
     if isinstance(sprGroup, GroupSingle):
         sprGroup.add(obj)
     elif isinstance(sprGroup, Group):
-        # ToDo
-        pass
+        self.kill()
+        sprGroup.add(obj)
     if perf == True:
         obj.create()
     # copy position
